3a4ce73126c81d78955779bf1a62bafc6487bfbd
Frolova Ludmila <frolova.ludmila@russianpost.ru>
2022-07-02 13:53:05 +0300

    Zadacha15

---
+++ Exam006/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+void Zadacha15()
+{
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("Да, выходной!");
+}
+else if (number > 7 || number < 1)
+{
+    Console.WriteLine("Ввод не верный");
+}
+else Console.WriteLine("Нет, будний день");
+}
+Zadacha15();
