39864cc1ec3e5a2ff8b1420a13febe14e109140e
Frolova Ludmila <frolova.ludmila@russianpost.ru>
2022-07-08 22:55:40 +0300

    Zadacha 25

---
+++ Exam010/Program.cs
@@ -0,0 +1,24 @@
+﻿void Zadacha25()
+{
+    //Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+    //и возводит число A в натуральную степень B.
+    //3, 5 -> 243 (3⁵)
+    //2, 4 -> 16
+
+    Random random = new Random();
+    int number = random.Next(2,3);
+    int exponent = random.Next(3,5);
+    Console.WriteLine($"Число {number} в степени {exponent} равно {Power(number, exponent)}");
+}
+
+int Power( int number, int exponent)
+{
+    int result = 1;
+
+    for (int i = 0; i < exponent; i++)
+    {
+        result *= number;
+    }
+    return result;
+}
+Zadacha25();
