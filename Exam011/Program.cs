072547a19b1c715660ceae7dc2de91cf2680b6d0
Frolova Ludmila <frolova.ludmila@russianpost.ru>
2022-07-08 23:14:06 +0300

    Zadacha 27

---
+++ Exam011/Program.cs
@@ -0,0 +1,23 @@
+﻿void Zadacha27()
+{
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Random random = new Random();
+int number = random.Next (100,1000);
+
+int sum = 0;
+
+Console.Write("В числе " + number);
+while(number > 0)
+{
+    sum += number % 10;
+    number /=10;
+}
+
+Console.WriteLine("Сумма цифр равна: " + sum);
+
+}
+Zadacha27();
