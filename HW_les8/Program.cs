b6896ac6f12f628b813c4e5119b206ea2c10c0d7
Frolova Ludmila <frolova.ludmila@russianpost.ru>
2022-07-21 21:38:06 +0300

    Zadacha54, Zadacha56

--- HW_les8/Program.cs
+++ HW_les8/Program.cs
@@ -8,7 +8,7 @@
 //7 2 4 1
 //9 5 3 2
 //8 4 4 2
-void Zadacha54()
+//void Zadacha54()
 {
     Random random = new Random();
     int rows = random.Next(2, 5);
@@ -71,4 +71,88 @@ int[,] SortedArray(int[,] arr)
     }
     return arr;
 }
-Zadacha54();
+//Zadacha54();
+
+//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу,
+//которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей
+//  суммой элементов: 1 строка
+void Zadacha56()
+{
+    Random random = new Random();
+    int rows = random.Next(3, 5);
+    int columns = random.Next(5, 7);
+    int[,] array = new int[rows, columns];
+    CreateArray(array);
+    Console.WriteLine("Двумерный массив:");
+    PrintArray(array);
+    FillMinSum(array);
+    Console.WriteLine("Массив из сумм строк:");
+    PrintArrayOnes(FillMinSum(array));
+    Console.WriteLine();
+    Console.WriteLine("Минимальная сумма строки:");
+    int min = MinSum(FillMinSum(array));
+    Console.WriteLine(min);
+}
+int[,] CreateArray(int[,] arr)
+{
+    Random random = new Random();
+    int rows = arr.GetLength(0);
+    int columns = arr.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            arr[i, j] = random.Next(1,10);
+        }
+    }
+    return arr;
+}
+
+int[] PrintArrayOnes(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + "\t");
+    }
+    return arr;
+}
+
+int[] FillMinSum(int[,] arr)
+{
+    int rows = arr.GetLength(0);
+    int columns = arr.GetLength(1);
+    int[] array = new int[rows];
+    int index = 0;
+    int sum = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        sum = 0;
+        for (int j = 0; j < columns; j++)
+        {
+            sum += arr[i, j];
+        }
+        array[index] = sum;
+        index++;
+    }
+    return array;
+}
+
+int MinSum(int[] arr)
+{
+    int min = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+Zadacha56();
